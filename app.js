ab47c67c93ef730d253ae77b0f4f0ea0cab26709
Andy Weiss <andyweiss1982@gmail.com>
2019-10-15 10:37:19 -0400

    dont hard code count

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const bottles = document.querySelectorAll('.bottle')
 const counters = document.querySelectorAll('.count')
 const bottleWords = document.querySelectorAll('.bottle-word')
-let remainingBottles = 99
+
+function count(){
+  const remainingBottles = document.querySelectorAll('.bottle:not(.drank)').length
+  counters.forEach(counter => counter.textContent = remainingBottles)
+  const word = remainingBottles === 1 ? 'bottle' : 'bottles'
+  bottleWords.forEach(bottleWord => bottleWord.textContent = word)
+}
 
 function drink(){
   if(confirm('Take one down, pass it around?')){
     this.classList.add('drank')
-    remainingBottles --
-    counters.forEach(counter => counter.textContent = remainingBottles)
-    const word = remainingBottles === 1 ? 'bottle' : 'bottles'
-    bottleWords.forEach(bottleWord => bottleWord.textContent = word)
+    count()
   }
 }
 
 bottles.forEach(bottle => bottle.addEventListener('click', drink))
+
+count()
