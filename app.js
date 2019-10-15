31c59792386636e1b8617d8abd2935fe10121d3e
Andy Weiss <andyweiss1982@gmail.com>
2019-10-15 10:40:38 -0400

    move to one function

--- app.js
+++ app.js
@@ -2,20 +2,14 @@ const bottles = document.querySelectorAll('.bottle')
 const counters = document.querySelectorAll('.count')
 const bottleWords = document.querySelectorAll('.bottle-word')
 
-function count(){
-  const remainingBottles = document.querySelectorAll('.bottle:not(.drank)').length
-  counters.forEach(counter => counter.textContent = remainingBottles)
-  const word = remainingBottles === 1 ? 'bottle' : 'bottles'
-  bottleWords.forEach(bottleWord => bottleWord.textContent = word)
-}
-
 function drink(){
   if(confirm('Take one down, pass it around?')){
     this.classList.add('drank')
-    count()
+    const remainingBottles = document.querySelectorAll('.bottle:not(.drank)').length
+    counters.forEach(counter => counter.textContent = remainingBottles)
+    const word = remainingBottles === 1 ? 'bottle' : 'bottles'
+    bottleWords.forEach(bottleWord => bottleWord.textContent = word)
   }
 }
 
 bottles.forEach(bottle => bottle.addEventListener('click', drink))
-
-count()
